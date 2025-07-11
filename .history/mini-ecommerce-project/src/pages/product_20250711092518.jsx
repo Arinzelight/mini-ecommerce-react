75fdbb1b6e0bc357b19dfa0b014690735b1b0ca1
Celebration-cloud <ojinguluc@gmail.com>
2025-07-11 13:13:06 +0100

    cleaning history

--- .history/mini-ecommerce-project/src/pages/product_20250711092518.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ProductPage() {
-  return (
-    <div><h3></h3>ProductPage</div>
-  )
-}
-
-export default ProductPage
--- .history/mini-ecommerce-project/src/pages/product_20250711092526.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ProductPage() {
-  return (
-    <div><h3>ProductPage</h3></div>
-  )
-}
-
-export default ProductPage
